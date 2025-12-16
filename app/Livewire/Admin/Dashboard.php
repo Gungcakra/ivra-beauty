d4c5dfdc76b788504ef60b96960b1500ebc79f8b
Gungcakra <agungcakra888@gmail.com>
2025-12-16 17:06:54 +0800

    add dashboard

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin;
 
 use App\Models\Menu;
+use App\Models\Transaksi;
 use Livewire\Component;
 use Livewire\Attributes\Layout;
 use Carbon\Carbon;
@@ -21,7 +22,16 @@ class Dashboard extends Component
     }
     public function render()
     {
-        return view('livewire.pages.admin.dashboard');
+        $thisMonthTransaction = Transaksi::where('created_at', '>=', Carbon::now()->startOfMonth())
+            ->where('created_at', '<=', Carbon::now()->endOfMonth())
+            ->get();
+        $thisMonthIncome = $thisMonthTransaction->sum('nominal_pembayaran');
+        $thisMonthReservasiCount = $thisMonthTransaction->count();
+        $viewData = [
+            'thisMonthIncome' => $thisMonthIncome,
+            'thisMonthReservasiCount' => $thisMonthReservasiCount,
+        ];
+        return view('livewire.pages.admin.dashboard', $viewData);
     }
 
 }
